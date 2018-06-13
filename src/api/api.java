e30b3dc4f148cf2f96ef243a1cbfc26e888312f6
why <2488062283qq.com>
2018-06-13 16:38:32 +0800

    api

--- src/api/api.java
+++ src/api/api.java
@@ -1,4 +1,5 @@
-﻿/**
+﻿
+/**
  *
  * User：
 1. 登录：URL /user_login (post) {user}
@@ -8,9 +9,9 @@
 5. 查询所有................user_getAll(get)
 6. 查询部分................user_getPart (get)
 7. 退出.......................user_logout  (get)
-5. post放入
+8. post放入
 参数：user(u_id(int),username,password,email,role,code)
-6.返回信息：
+9.返回信息：
 	         code:0,1,2,3
 	         msg:xx 
 
@@ -21,9 +22,9 @@ Recruit：
 4. 查询所有：...............recruit_getAll(get)
 5. 查询单个：...............recruit_get (get)
 6. 搜索：...............recruit_getPart (get)
-4. post放入
+7. post放入
 参数：recruit(r_id，name，sex，school，filepath，state（int）)
-5.返回信息：
+8.返回信息：
          code:0
          msg:xxx
 employee：
@@ -33,10 +34,10 @@ employee：
       4. 查询所有：URL /employee_getAll (get) 
       5. 查询：.........employee_get (get)
       6. 查询部分：..........employee_getPart (get)
-4. post放入
-   参数：employee(e_id,name，age（int）,sex,marry,idCard,edu,school,mobile,
- address,email,department,salary)
-5.返回信息：
+      7. post放入
+                            参数：employee(e_id,name，age（int）,sex,marry,idCard,edu,school,mobile,
+         address,email,department(Department),salary(Salary))
+      8.返回信息：
          code:0
          msg:xxx
 
@@ -47,11 +48,29 @@ department：
       4. 查询所有：.............department_getAll (get) 
       5. 查询部分：.................department_getPart (get)
       6. 查询单个：...............department_getPart (get)
-4. post放入
-   参数： department(d_id ,name，manager，manyEmployee)
-5. 返回信息：
+      7. post放入
+                             参数： department(d_id ,name，manager，manyEmployee(Set<Employee>))
+      8. 返回信息：
            code:0
            msg:xxx
+EmployeeAndDepartment综合相关：
+      1. 查询所有员工和部门：URL /employeeAnddepartment_getAllEmployeeAndDepartment  (get)
+      2. 查询：URL /employeeAnddepartment_getSize_page  (get)
+      3. post放入（暂时未用上post）
+                  参数：employee(e_id,name，age（Integer）,sex,marry,idCard,edu,school,mobile,
+      address,email,department)
+Salary：
+      1. 添加： URL /...addSalary  (post) {salary}
+      2. 删除：URL /...romoveSalary  (post) {s_id}
+      3. 更新： URL /...updateSalary  (post) {salary}
+      4. 查询所有：URL /...updateSalary (get)
+      5. 查询部分： URL /...getPartSalary (get)
+      6. 查询单人：URL /...getSalary (get)
+      7. post放入：
+                           参数：salary(s_id,name,salary,bonus(Double),employee(Employee))
+      8. 返回信息：
+            code:0
+            msg:xxx
  * 
 **/
 
